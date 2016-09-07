5a981701d243b57ba19f0080477a674a4e9dbd13
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-07 10:04:48 -0700

    added company and company_address

--- postgres/tables.sql
+++ postgres/tables.sql
@@ -1,11 +1,39 @@
 -- Stocks 
-CREATE TABLE stocks
+CREATE TABLE stock
 (
-	stock_id character varying(20) NOT NULL,
+	symbol character varying(20) NOT NULL,
 	price numeric NOT NULL,
 	last_update timestamp with time zone NOT NULL DEFAULT now(),
-	CONSTRAINT stocks_pkey PRIMARY KEY (stock_id)
+	CONSTRAINT stocks_pkey PRIMARY KEY (symbol)
 );
 
-INSERT INTO stocks (stock_id, price)
+INSERT INTO stock (symbol, price)
 	VALUES ('ORCL', '41.06');
+
+-- Company
+-- one company can have multiple stock symbols, not 1:1?
+CREATE TABLE company 
+(
+	company_id integer NOT NULL DEFAULT nextval('company_company_id_seq'::regclass),
+	name character varying(50) NOT NULL,
+	image character varying(100) NOT NULL,
+	year_founded numeric(4,0) NOT NULL,
+	CONSTRAINT company_pkey PRIMARY KEY (company_id),
+)
+
+-- Company Address
+CREATE TABLE company_address
+(
+	company_address_id integer NOT NULL DEFAULT nextval('company_company_id_seq'::regclass),
+	company_id integer NOT NULL DEFAULT,
+	address_id integer NOT NULL DEFAULT,
+	CONSTRAINT company_address_pkey PRIMARY KEY (company_address_id, company_id, address_id),
+	CONSTRAINT fk_company_address FOREIGN KEY (company_id)
+		REFERENCES public.company (company_id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+	CONSTRAINT fk_company_address FOREIGN KEY (address_id)
+		REFERENCES public.address (company_id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
+-- Address
