673564c937fbbbe139f20cbee0de463b3fada08f
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-09-11 13:54:06 -0700

    added company news and news tables

--- postgres/tables.sql
+++ postgres/tables.sql
@@ -56,6 +56,24 @@ CREATE TABLE country
 	last_update timestamp with time zone NOT NULL DEFAULT now()
 );
 
+-- Company_News
+CREATE TABLE company_news
+(
+	company_news_id serial,
+	company_id integer NOT NULL, -- fk company(company_id)
+	news_id integer NOT NULL, -- fk news(news_id)
+	last_update timestamp with time zone NOT NULL DEFAULT now()
+);
+
+-- News
+CREATE TABLE news
+(
+	news_id serial PRIMARY KEY,
+	article character varying(50) NOT NULL,
+	description character varying(200) NOT NULL,
+	last_update timestamp with time zone NOT NULL DEFAULT now()
+);
+
 -- Alter tables to add Foreign Keys
 
 -- company alter
@@ -76,3 +94,9 @@ ADD FOREIGN KEY (address_id) REFERENCES address (address_id);
 -- city alter
 ALTER TABLE city
 ADD FOREIGN KEY (country_id) REFERENCES country (country_id);
+
+-- Company News alter
+ALTER TABLE company_news
+ADD CONSTRAINT company_news_pkey PRIMARY KEY (company_news_id, company_id, news_id), -- composite key
+ADD FOREIGN KEY (company_id) REFERENCES company (company_id),
+ADD FOREIGN KEY (news_id) REFERENCES news (news_id);
